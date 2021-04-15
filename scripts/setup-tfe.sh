734a6267246521da2f0e273f0319ff37ac7a3a45
Anton Akulov <aakulov@hashicorp.com>
2021-04-15 12:06:48 +0200

    Improved install script

--- scripts/setup-tfe.sh
+++ scripts/setup-tfe.sh
@@ -3,7 +3,8 @@ echo "# Downloading installation script"
 curl -# -o $HOME/install.sh https://install.terraform.io/ptfe/stable
 cp /home/vagrant/replicated.conf /etc/replicated.conf
 cp /home/vagrant/replicated.conf /root/replicated.conf
-echo "# Installing docker"
-apt-get -y install docker.io
+echo "# Fixing a repository certificate issue"
+apt-get -y install dirmngr gpg-agent
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DA418C88A3219F7B
 echo "# Running installation script"
-bash $HOME/install.sh airgap no-proxy private-address=192.168.56.33 public-address=192.168.56.33
+bash $HOME/install.sh no-proxy private-address=192.168.56.33 public-address=192.168.56.33
